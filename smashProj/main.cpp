4cf067d4199ef79b7d4ec0790ed32692091cfe04
Peter Davids <peterdavids@PMBP.local>
2015-06-20 10:29:32 -0500

    Initial Commit

---
+++ smashProj/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  smashProj
+//
+//  Created by Peter Davids on 6/20/15.
+//  Copyright (c) 2015 Peter Davids. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
